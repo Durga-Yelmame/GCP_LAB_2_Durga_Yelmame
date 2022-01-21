66cd23ab6ad9ec867275d505a9be9100c14ace24
Durga-Yelmame <96398013+Durga-Yelmame@users.noreply.github.com>
2022-01-21 17:26:11 +0530

    LAB2 Done.

--- LAB2_Code_SQL_Files/Durga_Yelmame_LAB2_Exercise_4.sql
+++ LAB2_Code_SQL_Files/Durga_Yelmame_LAB2_Exercise_4.sql
@@ -1,4 +1,5 @@
-SELECT * FROM(
+SELECT  country,ARRAY_AGG(STRUCT(operatingSystem ,browser,rank)LIMIT 3) as country_rank
+   FROM(
 SELECT geoNetwork.country as country,
  ARRAY_AGG(device.operatingSystem) OVER ( PARTITION BY geoNetwork.country
  ORDER BY (
@@ -18,10 +19,11 @@ SELECT geoNetwork.country as country,
  group by geoNetwork.country, device.operatingSystem, device.browser
 
   ) asc ) AS browser,
-
+ ROW_NUMBER()  OVER (PARTITION By geoNetwork.country ORDER BY (count(device.browser)*count(device.operatingSystem)) DESC) AS rank
  FROM `bigquery-public-data.google_analytics_sample.ga_sessions_20170801` as oq 
  WHERE device.isMobile=true and geoNetwork.country!="(not set)" and device.operatingSystem!="(not set)" and   device.browser!="(not set)"
  GROUP BY geoNetwork.country, device.operatingSystem, device.browser)
+ GROUP BY country
  ORDER BY country ASC
 
 
